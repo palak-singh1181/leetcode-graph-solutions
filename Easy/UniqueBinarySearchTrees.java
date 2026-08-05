739bcbacf7c00a60d3b32f084e6716341ff92ba3
Palak Singh <palaksingh9503@gmail.com>
2026-08-05 09:51:17 -0700

    Added this solution of leetcode

---
+++ Easy/UniqueBinarySearchTrees.java
@@ -0,0 +1,28 @@
+
+public class UniqueBinarySearchTrees {
+
+    public static int numTrees(int n) {
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+
+            for (int root = 1; root <= nodes; root++) {
+
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        System.out.println(numTrees(n));
+    }
+}
